3cbaebc5b66e75606838fc118d0e7cde20d6edb3
amy-fisher <115939629+amy-fisher@users.noreply.github.com>
2023-05-25 17:05:25 +0100

    Update Data6.4.cs
    updated ocean fishing bites & fish that are confirmed one bait only

--- GatherBuddy.GameData/Data/Fish/Data6.4.cs
+++ GatherBuddy.GameData/Data/Fish/Data6.4.cs
@@ -35,165 +35,245 @@ public static partial class Fish
         data.Apply(39912, Patch.TheDarkThrone) // The Fury's Aegis
             .Bait(data);
         data.Apply(40521, Patch.TheDarkThrone) // Pink Shrimp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40522, Patch.TheDarkThrone) // Sirensong Mussel
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40523, Patch.TheDarkThrone) // Arrowhead
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40524, Patch.TheDarkThrone) // Deepshade Sardine
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40525, Patch.TheDarkThrone) // Sirensong Mullet
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40526, Patch.TheDarkThrone) // Selkie Puffer
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40527, Patch.TheDarkThrone) // Poet's Pipe
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40528, Patch.TheDarkThrone) // Marine Matanga
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40529, Patch.TheDarkThrone) // Spectral Coelacanth
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40530, Patch.TheDarkThrone) // Dusk Shark
-            .Bait(data);
+            .Bait(data, 29716)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40531, Patch.TheDarkThrone) // Mermaid Scale
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40532, Patch.TheDarkThrone) // Broadhead
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40533, Patch.TheDarkThrone) // Vivid Pink Shrimp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40534, Patch.TheDarkThrone) // Sunken Coelacanth
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40535, Patch.TheDarkThrone) // Siren's Sigh
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40536, Patch.TheDarkThrone) // Black-jawed Helicoprion
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40537, Patch.TheDarkThrone) // Impostopus
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40538, Patch.TheDarkThrone) // Jade Shrimp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40539, Patch.TheDarkThrone) // Nymeia's Wheel
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40540, Patch.TheDarkThrone) // Taniwha
-            .Bait(data);
+            .Bait(data, 36593)
+            .Bite(HookSet.Unknown, BiteType.Legendary)
+            .Ocean(OceanTime.Day);
         data.Apply(40541, Patch.TheDarkThrone) // Ruby Herring
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40542, Patch.TheDarkThrone) // Whirpool Turban
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40543, Patch.TheDarkThrone) // Leopard Prawn
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40544, Patch.TheDarkThrone) // Spear Squid
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40545, Patch.TheDarkThrone) // Floating Lantern
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40546, Patch.TheDarkThrone) // Rubescent Tatsunoko
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40547, Patch.TheDarkThrone) // Hatatate
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40548, Patch.TheDarkThrone) // Silent Shark
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40549, Patch.TheDarkThrone) // Spectral Wrasse
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40550, Patch.TheDarkThrone) // Mizuhiki
-            .Bait(data);
+            .Bait(data, 29714)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40551, Patch.TheDarkThrone) // Snapping Koban
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40552, Patch.TheDarkThrone) // Silkweft Prawn
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40553, Patch.TheDarkThrone) // Stingfin Trevally
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40554, Patch.TheDarkThrone) // Swordtip Squid
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40555, Patch.TheDarkThrone) // Mailfish
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40556, Patch.TheDarkThrone) // Idaten's Bolt
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40557, Patch.TheDarkThrone) // Maelstrom Turban
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40558, Patch.TheDarkThrone) // Shoshitsuki
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40559, Patch.TheDarkThrone) // Spadefish
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40560, Patch.TheDarkThrone) // Glass Dragon
-            .Bait(data);
+            .Bait(data, 29715, 40551)
+            .Bite(HookSet.Powerful, BiteType.Legendary)
+            .Ocean(OceanTime.Night);
         data.Apply(40561, Patch.TheDarkThrone) // Crimson Kelp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40562, Patch.TheDarkThrone) // Reef Squid
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40563, Patch.TheDarkThrone) // Pinebark Flounder
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40564, Patch.TheDarkThrone) // Mantle Moray
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40565, Patch.TheDarkThrone) // Shisui Goby
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40566, Patch.TheDarkThrone) // Sanbaso
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40567, Patch.TheDarkThrone) // Barded Lobster
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40568, Patch.TheDarkThrone) // Violet Sentry
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40569, Patch.TheDarkThrone) // Spectral Snake Eel
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40570, Patch.TheDarkThrone) // Heavensent Shark
-            .Bait(data);
+            .Bait(data, 29715)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40571, Patch.TheDarkThrone) // Fleeting Squid
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40572, Patch.TheDarkThrone) // Bowbarb Lobster
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40573, Patch.TheDarkThrone) // Pitch Pickle
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40574, Patch.TheDarkThrone) // Senbei Octopus
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40575, Patch.TheDarkThrone) // Tentacle Thresher
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40576, Patch.TheDarkThrone) // Bekko Rockhugger
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40577, Patch.TheDarkThrone) // Yellow Iris
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40578, Patch.TheDarkThrone) // Crimson Sentry
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40579, Patch.TheDarkThrone) // Flying Squid
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40580, Patch.TheDarkThrone) // Hells' Claw
-            .Bait(data);
+            .Bait(data, 27590)
+            .Bite(HookSet.Unknown, BiteType.Legendary)
+            .Ocean(OceanTime.Sunset);
         data.Apply(40581, Patch.TheDarkThrone) // Catching Carp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40582, Patch.TheDarkThrone) // Garlean Bluegill
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40583, Patch.TheDarkThrone) // Yanxian Softshell
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40584, Patch.TheDarkThrone) // Princess Salmon
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40585, Patch.TheDarkThrone) // Calligraph
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40586, Patch.TheDarkThrone) // Singular Shrimp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40587, Patch.TheDarkThrone) // Brocade Carp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Powerful, BiteType.Strong);
         data.Apply(40588, Patch.TheDarkThrone) // Yanxian Sturgeon
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40589, Patch.TheDarkThrone) // Spectral Kotsu Zetsu
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40590, Patch.TheDarkThrone) // Fishy Shark
-            .Bait(data);
+            .Bait(data, 29716)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40591, Patch.TheDarkThrone) // Gensui Shrimp
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40592, Patch.TheDarkThrone) // Yato-no-kami
             .Bait(data);
         data.Apply(40593, Patch.TheDarkThrone) // Heron's Eel
             .Bait(data);
         data.Apply(40594, Patch.TheDarkThrone) // Crowshadow Mussel
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40595, Patch.TheDarkThrone) // Yanxian Goby
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40596, Patch.TheDarkThrone) // Iridescent Trout
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Precise, BiteType.Weak);
         data.Apply(40597, Patch.TheDarkThrone) // Un-Namazu
             .Bait(data);
         data.Apply(40598, Patch.TheDarkThrone) // Gakugyo
-            .Bait(data);
+            .Bait(data)
+            .Bite(HookSet.Unknown, BiteType.Legendary);
         data.Apply(40599, Patch.TheDarkThrone) // Ginrin Goshiki
             .Bait(data);
         data.Apply(40600, Patch.TheDarkThrone) // Jewel of Plum Spring
-            .Bait(data);
+            .Bait(data, 12704)
+            .Bite(HookSet.Unknown, BiteType.Legendary)
+            .Ocean(OceanTime.Day);
     }
     // @formatter:on
 }
