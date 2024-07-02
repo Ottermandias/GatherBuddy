2aa7d12426d02038ec22aeff7c4ab5256d92a8e3
Ottermandias <nickroederspam+git1@gmail.com>
2024-07-02 08:53:29 +0200

    Fix chatlog context item offset.

--- GatherBuddy/Plugin/Offsets.cs
+++ GatherBuddy/Plugin/Offsets.cs
@@ -10,5 +10,5 @@ public static class Offsets
     public const int AgentItemContextItemId          = 0x28;
     public const int GatheringNoteContextItemId      = 0xA0;
     public const int ItemSearchContextItemId         = 0x1738;
-    public const int ChatLogContextItemId            = 0x948;
+    public const int ChatLogContextItemId            = 0x950;
 }
