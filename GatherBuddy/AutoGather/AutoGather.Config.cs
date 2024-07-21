b9347b57ca77f86b63b11cd8dc3b9cfd4cf6b8e6
Aelys <Aelys@mayako.com>
2024-07-21 22:17:39 +0200

    Added collectible gathering on the last integrity point

--- GatherBuddy/AutoGather/AutoGather.Config.cs
+++ GatherBuddy/AutoGather/AutoGather.Config.cs
@@ -13,25 +13,27 @@ namespace GatherBuddy.AutoGather
         public uint                            AutoGatherMountId             { get; set; } = 1;
         public Dictionary<uint, List<Vector3>> BlacklistedNodesByTerritoryId { get; set; } = new();
 
-        public ActionConfig BYIIConfig                 { get; set; } = new(true, 100, uint.MaxValue);
-        public ActionConfig LuckConfig                 { get; set; } = new(true, 200, uint.MaxValue);
-        public ActionConfig YieldIIConfig              { get; set; } = new(true, 500, uint.MaxValue);
-        public ActionConfig YieldIConfig               { get; set; } = new(true, 400, uint.MaxValue);
-        public ActionConfig ScrutinyConfig             { get; set; } = new(true, (uint)AutoGather.Actions.Scrutiny.GpCost, uint.MaxValue);
-        public ActionConfig MeticulousConfig           { get; set; } = new(true, (uint)AutoGather.Actions.Meticulous.GpCost, uint.MaxValue);
-        public ActionConfig SolidAgeConfig             { get; set; } = new(true, (uint)AutoGather.Actions.SolidAge.GpCost, uint.MaxValue);
-        public ActionConfig WiseConfig                 { get; set; } = new(true, (uint)AutoGather.Actions.Wise.GpCost, uint.MaxValue);
-        public ActionConfig CollectConfig              { get; set; } = new(true, (uint)AutoGather.Actions.Collect.GpCost, uint.MaxValue);
-        public ActionConfig ScourConfig                { get; set; } = new(true, (uint)AutoGather.Actions.Scour.GpCost, uint.MaxValue);
-        public int          TimedNodePrecog            { get; set; } = 120;
-        public bool         DoGathering                { get; set; } = true;
-        public uint         MinimumGPForGathering      { get; set; } = 0;
-        public float        NavResetCooldown           { get; set; } = 3.0f;
-        public float        NavResetThreshold          { get; set; } = 2.0f;
-        public bool         ForceWalking               { get; set; } = false;
-        public float        FarNodeFilterDistance      { get; set; } = 50.0f;
-        public bool         DisableFlagPathing         { get; set; } = false;
-        public uint         MinimumCollectibilityScore { get; set; } = 1000;
+        public ActionConfig BYIIConfig                                 { get; set; } = new(true, 100, uint.MaxValue);
+        public ActionConfig LuckConfig                                 { get; set; } = new(true, 200, uint.MaxValue);
+        public ActionConfig YieldIIConfig                              { get; set; } = new(true, 500, uint.MaxValue);
+        public ActionConfig YieldIConfig                               { get; set; } = new(true, 400, uint.MaxValue);
+        public ActionConfig ScrutinyConfig                             { get; set; } = new(true, (uint)AutoGather.Actions.Scrutiny.GpCost, uint.MaxValue);
+        public ActionConfig MeticulousConfig                           { get; set; } = new(true, (uint)AutoGather.Actions.Meticulous.GpCost, uint.MaxValue);
+        public ActionConfig SolidAgeConfig                             { get; set; } = new(true, (uint)AutoGather.Actions.SolidAge.GpCost, uint.MaxValue);
+        public ActionConfig WiseConfig                                 { get; set; } = new(true, (uint)AutoGather.Actions.Wise.GpCost, uint.MaxValue);
+        public ActionConfig CollectConfig                              { get; set; } = new(true, (uint)AutoGather.Actions.Collect.GpCost, uint.MaxValue);
+        public ActionConfig ScourConfig                                { get; set; } = new(true, (uint)AutoGather.Actions.Scour.GpCost, uint.MaxValue);
+        public int          TimedNodePrecog                            { get; set; } = 120;
+        public bool         DoGathering                                { get; set; } = true;
+        public uint         MinimumGPForGathering                      { get; set; } = 0;
+        public float        NavResetCooldown                           { get; set; } = 3.0f;
+        public float        NavResetThreshold                          { get; set; } = 2.0f;
+        public bool         ForceWalking                               { get; set; } = false;
+        public float        FarNodeFilterDistance                      { get; set; } = 50.0f;
+        public bool         DisableFlagPathing                         { get; set; } = false;
+        public uint         MinimumCollectibilityScore                 { get; set; } = 1000;
+        public bool         GatherIfLastIntegrity                      { get; set; } = false;
+        public uint         GatherIfLastIntegrityMinimumCollectibility { get; set; } = 600;
 
         public class ActionConfig
         {
