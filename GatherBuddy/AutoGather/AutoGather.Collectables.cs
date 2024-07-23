427bd0f506cd4f9db7b06a762ca4d88ec9c621ee
C.Hat32 <killonera@gmail.com>
2024-07-23 17:24:03 +0200

    Reworked the Collectables actions conditions and logic to improve decision making and allow to choose when to do a full collect rotation

---
+++ GatherBuddy/AutoGather/AutoGather.Collectables.cs
@@ -0,0 +1,118 @@
+using ECommons.GameHelpers;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using System.Linq;
+
+namespace GatherBuddy.AutoGather
+{
+    public partial class AutoGather
+    {
+        private static CollectableRotation? CurrentRotation;
+        private unsafe class CollectableRotation
+        {
+            public CollectableRotation(uint GPToStart)
+            {
+                shouldUseFullRotation = Player.Object.CurrentGp >= GPToStart;
+            }
+            
+            private bool shouldUseFullRotation = false;
+            
+            public Actions.BaseAction GetNextAction(AddonGatheringMasterpiece* MasterpieceAddon)
+                => shouldUseFullRotation ? FullRotation(MasterpieceAddon) : FillerRotation(MasterpieceAddon);
+
+            private Actions.BaseAction FullRotation(AddonGatheringMasterpiece* MasterpieceAddon)
+            {
+                int collectability   = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(6)->NodeText.ToString());
+                int currentIntegrity = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(126)->NodeText.ToString());
+                int maxIntegrity     = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(129)->NodeText.ToString());
+                int scourColl        = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(84)->NodeText.ToString().Substring(2));
+                int meticulousColl   = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(108)->NodeText.ToString().Substring(2));
+                int brazenColl       = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(93)->NodeText.ToString().Substring(2));
+                
+                if (currentIntegrity < maxIntegrity && ShouldUseWise())
+                    return Actions.Wise;
+                
+                if (collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore)
+                {
+                    if (currentIntegrity <= maxIntegrity
+                     && ShouldUseSolidAge(currentIntegrity))
+                        return Actions.SolidAge;
+                    
+                    return Actions.Collect;
+                }
+
+                if (currentIntegrity == 1
+                 && GatherBuddy.Config.AutoGatherConfig.GatherIfLastIntegrity
+                 && collectability >= GatherBuddy.Config.AutoGatherConfig.GatherIfLastIntegrityMinimumCollectibility)
+                    return Actions.Collect;
+
+                if (NeedScrutiny(collectability, scourColl, meticulousColl, brazenColl) && ShouldUseScrutiny())
+                    return Actions.Scrutiny;
+
+                if (meticulousColl + collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore
+                 && ShouldUseMeticulous())
+                    return Actions.Meticulous;
+
+                if (Dalamud.ClientState.LocalPlayer.StatusList.Any(s => s.StatusId == 3911)
+                 && brazenColl + collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore
+                 && ShouldUseBrazen())
+                    return Actions.Brazen;
+
+                if (scourColl + collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore
+                 && ShouldUseScour())
+                    return Actions.Scour;
+
+                if(ShouldUseMeticulous())
+                    return Actions.Meticulous;
+
+                return Actions.Scour;
+            }
+            
+            private Actions.BaseAction FillerRotation(AddonGatheringMasterpiece* MasterpieceAddon)
+            {
+                int collectability   = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(6)->NodeText.ToString());
+                int currentIntegrity = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(126)->NodeText.ToString());
+                int scourColl        = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(84)->NodeText.ToString().Substring(2));
+                int meticulousColl   = int.Parse(MasterpieceAddon->AtkUnitBase.GetTextNodeById(108)->NodeText.ToString().Substring(2));
+                
+                if (collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore)
+                    return Actions.Collect;
+
+                if (currentIntegrity == 1
+                 && GatherBuddy.Config.AutoGatherConfig.GatherIfLastIntegrity
+                 && collectability >= GatherBuddy.Config.AutoGatherConfig.GatherIfLastIntegrityMinimumCollectibility)
+                    return Actions.Collect;
+
+                if (meticulousColl + collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore
+                 && ShouldUseMeticulous())
+                    return Actions.Meticulous;
+
+                if (scourColl + collectability >= GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore
+                 && ShouldUseScour())
+                    return Actions.Scour;
+                
+                if (Dalamud.ClientState.LocalPlayer.StatusList.Any(s => s.StatusId == 3911))
+                    return Actions.Brazen;
+
+                if(ShouldUseMeticulous())
+                    return Actions.Meticulous;
+
+                return Actions.Scour;
+            }
+
+            private bool NeedScrutiny(int collectability, int scourColl, int meticulousColl, int brazenColl)
+            {
+                uint collAim = GatherBuddy.Config.AutoGatherConfig.MinimumCollectibilityScore;
+                if (scourColl + collectability >= collAim)
+                    return false;
+                if (meticulousColl + collectability >= collAim)
+                    return false;
+                if (Dalamud.ClientState.LocalPlayer.StatusList.Any(s => s.StatusId == 3911)
+                 && brazenColl + collectability >= collAim)
+                    return false;
+
+                return true;
+            }
+        }
+    }
+
+}
