81997181b4ff4b90f14daadae0de972bcfe74585
oorzkws <65210810+oorzkws@users.noreply.github.com>
2024-08-17 16:20:42 -0600

    Fix idle teleporting from inside estate
    Lifestream can be set to auto-enter estate and this breaks our previous behavior

---
+++ GatherBuddy/SeFunctions/HousingManager.cs
@@ -0,0 +1,19 @@
+﻿using SeHousingManager = FFXIVClientStructs.FFXIV.Client.Game.HousingManager;
+
+namespace GatherBuddy.SeFunctions;
+
+public static unsafe class HousingManager
+{
+    public static bool IsInHousing()
+    {
+        var housingManager = SeHousingManager.Instance();
+        if (housingManager == null)
+            return false;
+
+        ref var housingTerritory = ref housingManager->CurrentTerritory;
+        if (housingTerritory == null)
+            return false;
+        
+        return true;
+    }
+}
