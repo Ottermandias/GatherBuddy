0423dc14095acb30ca6ffe7f4f3c4bb9a188bbf3
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2024-08-18 16:15:55 +0100

    Added GatherBuddyIpc.Export
    - allow other plugins to trigger export of fishing logs via IPC
    - bump IpcVersion

--- GatherBuddy/Plugin/GatherBuddyIpc.cs
+++ GatherBuddy/Plugin/GatherBuddyIpc.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using System.IO;
 using Dalamud.Plugin.Ipc;
 
 namespace GatherBuddy.Plugin;
 
 public class GatherBuddyIpc : IDisposable
 {
-    public const int    IpcVersion   = 1;
+    public const int    IpcVersion   = 2;
     public const string VersionName  = $"{GatherBuddy.InternalName}.Version";
     public const string IdentifyName = $"{GatherBuddy.InternalName}.Identify";
+    public const string ExportName   = $"{GatherBuddy.InternalName}.Export";
 
     private readonly  GatherBuddy  _plugin;
-    internal readonly ICallGateProvider<int>?          _versionProvider;
-    internal readonly ICallGateProvider<string, uint>? _identifyProvider;
+    internal readonly ICallGateProvider<int>?                  _versionProvider;
+    internal readonly ICallGateProvider<string, uint>?         _identifyProvider;
+    internal readonly ICallGateProvider<string, string, bool>? _exportProvider;
 
     public GatherBuddyIpc(GatherBuddy plugin)
     {
@@ -28,9 +31,9 @@ public class GatherBuddyIpc : IDisposable
             GatherBuddy.Log.Error($"Could not obtain provider for {VersionName}:\n{e}");
         }
 
-        _identifyProvider = Dalamud.PluginInterface.GetIpcProvider<string, uint>(IdentifyName);
         try
         {
+            _identifyProvider = Dalamud.PluginInterface.GetIpcProvider<string, uint>(IdentifyName);
             _identifyProvider.RegisterFunc(Identify);
         }
         catch (Exception e)
@@ -38,6 +41,18 @@ public class GatherBuddyIpc : IDisposable
             _identifyProvider = null;
             GatherBuddy.Log.Error($"Could not obtain provider for {IdentifyName}:\n{e}");
         }
+
+        try
+        {
+            _exportProvider = Dalamud.PluginInterface.GetIpcProvider<string, string, bool>(ExportName);
+            _exportProvider.RegisterFunc(Export);
+        }
+        catch (Exception e)
+        {
+            _exportProvider = null;
+            GatherBuddy.Log.Error($"Could not obtain provider for {ExportName}:\n{e}");
+        }
+
     }
 
     private static int Version()
@@ -46,6 +61,26 @@ public class GatherBuddyIpc : IDisposable
     private uint Identify(string text)
         => _plugin.Executor.Identificator.IdentifyGatherable(text)?.ItemId
          ?? _plugin.Executor.Identificator.IdentifyFish(text)?.ItemId ?? 0;
+
+    private bool Export(string path, string format)
+    {
+        if (format.ToLower() == "json")
+        {
+            try
+            {
+                GatherBuddy.Log.Information($"Exporting FishRecorder :\n{path}");
+                var file = new FileInfo(path);
+                _plugin.FishRecorder.ExportJson(file);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
 
     public void Dispose()
     {
