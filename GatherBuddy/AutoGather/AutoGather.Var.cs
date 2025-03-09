012d64d4284fe9411db3945f042376cfa909046e
Alexander Ponyatykh <lazyranma@gmail.com>
2025-03-10 00:42:33 +0300

    Refactor UpdateItemsToGather() and make it respect job level

--- GatherBuddy/AutoGather/AutoGather.Var.cs
+++ GatherBuddy/AutoGather/AutoGather.Var.cs
@@ -107,85 +107,80 @@ namespace GatherBuddy.AutoGather
         private bool WentHome;
 
         public readonly List<GatherInfo> ItemsToGather = [];
-        public readonly Dictionary<ILocation, TimeInterval> VisitedTimedLocations = [];
+        public readonly Dictionary<GatheringNode, TimeInterval> VisitedTimedLocations = [];
         public readonly HashSet<Vector3> FarNodesSeenSoFar = [];
         public readonly LinkedList<uint> VisitedNodes = [];
         private GatherInfo? targetInfo = null;
 
         private IEnumerator<Actions.BaseAction?>? ActionSequence;
 
+        private static int MinerExpArrayIndex { get; } = Dalamud.GameData.GetExcelSheet<Lumina.Excel.Sheets.ClassJob>().GetRow((uint)Job.MIN).ExpArrayIndex;
+        private static int BotanistExpArrayIndex { get; } = Dalamud.GameData.GetExcelSheet<Lumina.Excel.Sheets.ClassJob>().GetRow((uint)Job.BTN).ExpArrayIndex;
+        private static unsafe int GetMinerLevel() => PlayerState.Instance()->ClassJobLevels[MinerExpArrayIndex];
+        private static unsafe int GetBotanistLevel() => PlayerState.Instance()->ClassJobLevels[BotanistExpArrayIndex];
+
         public void UpdateItemsToGather()
         {
             ItemsToGather.Clear();
-            var activeItems = OrderActiveItems(_plugin.AutoGatherListsManager.ActiveItems.OfType<Gatherable>().Select(GetBestLocation));
-            var RegularItemsToGather = new List<GatherInfo>();
-            foreach (var (item, location, time) in activeItems)
+            var activeItems = _plugin.AutoGatherListsManager.ActiveItems.OfType<Gatherable>()
+                // Not gathered enough.
+                .Where(item => InventoryCount(item) < QuantityTotal(item))
+                // If treasure map, only gather if we have none or the allowance is up.
+                .Where(item => !item.IsTreasureMap || InventoryCount(item) == 0 && NextTreasureMapAllowance < AdjustedServerTime.DateTime)
+                // Choose the best location and calculate the next uptime.
+                .Select(GetBestLocation)
+                // Filter out items that are not available right now or don't have an unvisited location.
+                .OfType<GatherInfo>()
+                // Prioritize timed nodes first.
+                .OrderBy(info => info.Time == TimeInterval.Always);
+
+            if (GatherBuddy.Config.AutoGatherConfig.SortingMethod == AutoGatherConfig.SortingType.Location)
             {
-                uint quest = item.GatheringType switch
-                {
-                    GatheringType.Miner    => 65728,
-                    GatheringType.Botanist => 65729,
-                    _                      => 0,
-                };
-                if (quest > 0 && !QuestManager.IsQuestComplete(quest))
-                {
-                    GatherBuddy.Log.Warning($"{item.Name} was requested but {item.GatheringType} is not unlocked");
-                    continue;
-                }
-                if (InventoryCount(item) >= QuantityTotal(item) || item.IsTreasureMap && InventoryCount(item) > 0)
-                    continue;
-
-                if (item.IsTreasureMap && NextTreasureMapAllowance >= AdjustedServerTime.DateTime)
-                    continue;
-
-                if (GatherBuddy.UptimeManager.TimedGatherables.Contains(item))
-                {
-                    if (time.InRange(AdjustedServerTime))
-                        ItemsToGather.Add((item, location, time));
-                }
-                else
-                {
-                    RegularItemsToGather.Add((item, location, time));
-                }
+                activeItems = activeItems
+                    // Order by node type.
+                    .ThenBy(info => GetNodeTypeAsPriority(info.Item))
+                    // Then by territory.
+                    .ThenBy(info => info.Location.Territory.Id);
             }
-            ItemsToGather.Sort((x, y) => GetNodeTypeAsPriority(x.Item).CompareTo(GetNodeTypeAsPriority(y.Item)));
-            ItemsToGather.AddRange(RegularItemsToGather);
+
+            ItemsToGather.AddRange(activeItems);
         }
 
-        private GatherInfo GetBestLocation(Gatherable item)
+        private GatherInfo? GetBestLocation(Gatherable item)
         {
-            (ILocation? Location, TimeInterval Time) res = default;
-            //First priority: selected preferred location.
-            var node = _plugin.AutoGatherListsManager.GetPreferredLocation(item);
-            var time = AdjustedServerTime;
-            if (node != null && !VisitedTimedLocations.ContainsKey(node))
-            {
-                res = (node, node.Times.NextUptime(time));
-            }
-            //Second priority: location for preferred job.
-            if ((res.Location == null || !res.Time.InRange(time)) && GatherBuddy.Config.PreferredGatheringType is GatheringType.Miner or GatheringType.Botanist)
-            {
-                res = GatherBuddy.UptimeManager.NextUptime(item, GatherBuddy.Config.PreferredGatheringType, time, [.. VisitedTimedLocations.Keys]);
-            }
-            //Otherwise: location for any job.
-            if (res.Location == null || !res.Time.InRange(time))
-            {
-                res = GatherBuddy.UptimeManager.NextUptime(item, time, [.. VisitedTimedLocations.Keys]);
-            }
-            return (item, (GatheringNode?)res.Location, res.Time);
-        }
+            // Items are unlocked in tiers of 5 levels, so we round up to the nearest 5.
+            var minerLevel = (GetMinerLevel() + 4) / 5 * 5;
+            var botanistLevel = (GetBotanistLevel() + 4) / 5 * 5;
+            // Preferred location from the list if set.
+            var pref = _plugin.AutoGatherListsManager.GetPreferredLocation(item);
+            var nodes = item.NodeList
+                // Remove nodes that have been visited.
+                .Except(VisitedTimedLocations.Keys)
+                // Remove nodes with level higher than the player can gather.
+                .Where(node => node.GatheringType.ToGroup() switch
+                {
+                    GatheringType.Miner => node.Level <= minerLevel,
+                    GatheringType.Botanist => node.Level <= botanistLevel,
+                    _ => false
+                })
+                // Get next uptime.
+                .Select(node => new GatherInfo(item, node, node.Times.NextUptime(AdjustedServerTime)))
+                // Filter out nodes that are not up. Also filter out invalid intervals.
+                .Where(info => info.Time.InRange(AdjustedServerTime))
+                // Prioritize preferred location, then preferred job, then the rest.
+                .OrderBy(info =>
+                {
+                    if (info.Location == pref)
+                        return 0;
+                    if (info.Location.GatheringType.ToGroup() == GatherBuddy.Config.PreferredGatheringType)
+                        return 1;
+                    return 2;
+                })
+                // Order by end time, longest first as in original UptimeManager.NextUptime().
+                .ThenByDescending(info => info.Time.End);
 
-        private IEnumerable<GatherInfo> OrderActiveItems(IEnumerable<GatherInfo> activeItems)
-        {
-            switch (GatherBuddy.Config.AutoGatherConfig.SortingMethod)
-            {
-                case AutoGatherConfig.SortingType.Location: return activeItems.OrderBy(i => i.Location?.Territory.Id);
-                case AutoGatherConfig.SortingType.None:
-                default:
-                    return activeItems;
-            }
+            return nodes.FirstOrDefault();
         }
-
 
         private unsafe T* GetAddon<T>(string name)
         {
@@ -252,19 +247,15 @@ namespace GatherBuddy.AutoGather
 
         private int GetNodeTypeAsPriority(Gatherable item)
         {
-            if (GatherBuddy.Config.AutoGatherConfig.SortingMethod == AutoGatherConfig.SortingType.None)
-                return 0;
-            Gatherable gatherable = item;
-            switch (gatherable?.NodeType)
+            return item.NodeType switch
             {
-                case NodeType.Legendary: return 0;
-                case NodeType.Unspoiled: return 1;
-                case NodeType.Ephemeral: return 2;
-                case NodeType.Regular:   return 9;
-                case NodeType.Unknown:   return 99;
-            }
-
-            return 99;
+                NodeType.Legendary => 0,
+                NodeType.Unspoiled => 1,
+                NodeType.Ephemeral => 2,
+                NodeType.Regular => 9,
+                NodeType.Unknown => 99,
+                _ => 99,
+            };
         }
 
         private static unsafe bool HasGivingLandBuff
@@ -292,16 +283,16 @@ namespace GatherBuddy.AutoGather
             => PlayerState.Instance()->Attributes[73];
 
         private ConfigPreset MatchConfigPreset(Gatherable? item) => _plugin.Interface.MatchConfigPreset(item);
-}
+    }
 
-    public record class GatherInfo(Gatherable Item, GatheringNode? Location, TimeInterval Time)
+    public record class GatherInfo(Gatherable Item, GatheringNode Location, TimeInterval Time)
     {
-        public static implicit operator (Gatherable Item, GatheringNode? Location, TimeInterval Time)(GatherInfo value)
+        public static implicit operator (Gatherable Item, GatheringNode Location, TimeInterval Time)(GatherInfo value)
         {
             return (value.Item, value.Location, value.Time);
         }
 
-        public static implicit operator GatherInfo((Gatherable Item, GatheringNode? Location, TimeInterval Time) value)
+        public static implicit operator GatherInfo((Gatherable Item, GatheringNode Location, TimeInterval Time) value)
         {
             return new GatherInfo(value.Item, value.Location, value.Time);
         }
