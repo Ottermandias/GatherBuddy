c0cc929149c526045dc142725544598ce0957f31
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2025-04-25 19:01:09 -0500

    Add GatherBuddy.DataImport project for fish data processing
    Introduced a new `GatherBuddy.DataImport` project to handle importing and merging fish records from JSON files. Updated `SimpleFishRecord` class to ensure equality and hashing based on unique IDs. Added fish_records.json as an embedded resource and configured the solution to include the new project.

---
+++ GatherBuddy.DataImport/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+using GatherBuddy.Models;
+using Newtonsoft.Json;
+
+namespace GatherBuddy.DataImport;
+
+class Program
+{
+    private const string OutputPath      = "../../../../GatherBuddy/CustomInfo";
+    private const string FishRecordsFile = "fish_records.json";
+    static void Main(string[] args)
+    {
+        var input = args[0];
+        if (!File.Exists(input))
+            throw new FileNotFoundException("Input file not found.", input);
+        var output = Path.Combine(OutputPath, FishRecordsFile);
+
+        var oldFishJson = File.ReadAllText(output);
+        var newFishJson = File.ReadAllText(input);
+
+        var oldFishRecords = JsonConvert.DeserializeObject<List<SimpleFishRecord>>(oldFishJson);
+        var newFishRecords = JsonConvert.DeserializeObject<List<SimpleFishRecord>>(newFishJson);
+
+        if (oldFishRecords == null || newFishRecords == null)
+            throw new Exception("Failed to deserialize json.");
+
+        var fishRecords = newFishRecords.Union(oldFishRecords).ToList();
+        File.WriteAllText(output, JsonConvert.SerializeObject(fishRecords, Formatting.Indented));
+    }
+}
--- GatherBuddy.Models/SimpleFishRecord.cs
+++ GatherBuddy.Models/SimpleFishRecord.cs
@@ -2,19 +2,25 @@
 
 public class SimpleFishRecord
 {
-    public Guid Id { get; set; }
-    public uint BaitId { get; set; }
-    public uint CatchId { get; set; }
-    public int Timestamp { get; set; }
-    public Effects Effects { get; set; }
-    public ushort Bite { get; set; }
-    public ushort Perception { get; set; }
-    public ushort Gathering { get; set; }
-    public ushort FishingSpot { get; set; }
-    public byte TugAndHook { get; set; }
-    public byte Amount { get; set; }
-    public float PositionX { get; set; }
-    public float PositionY { get; set; }
-    public float PositionZ { get; set; }
-    public float Rotation { get; set; }
+    public Guid    Id          { get; set; }
+    public uint    BaitId      { get; set; }
+    public uint    CatchId     { get; set; }
+    public int     Timestamp   { get; set; }
+    public Effects Effects     { get; set; }
+    public ushort  Bite        { get; set; }
+    public ushort  Perception  { get; set; }
+    public ushort  Gathering   { get; set; }
+    public ushort  FishingSpot { get; set; }
+    public byte    TugAndHook  { get; set; }
+    public byte    Amount      { get; set; }
+    public float   PositionX   { get; set; }
+    public float   PositionY   { get; set; }
+    public float   PositionZ   { get; set; }
+    public float   Rotation    { get; set; }
+
+    public override bool Equals(object? obj)
+        => obj is SimpleFishRecord record && Id == record.Id;
+
+    public override int GetHashCode()
+        => Id.GetHashCode();
 }
