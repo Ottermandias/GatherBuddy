0e87a11a2e9950914ab162e7c4706c912a6c6a14
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2025-05-03 06:05:49 -0500

    Add parameters to FishingAction and extend fishing actions
    Introduce `MinLevel`, `GpCost`, and `StatusProvide` parameters to `FishingAction` for better customization of fishing actions. Additionally, expand the list of fishing actions with detailed attributes to accommodate new functionality. This enhances the flexibility and clarity of the fishing system.

--- GatherBuddy/AutoGather/AutoGather.ActionIDs.cs
+++ GatherBuddy/AutoGather/AutoGather.ActionIDs.cs
@@ -10,9 +10,13 @@ public partial class AutoGather
 {
     public static class Actions
     {
-        public class FishingAction(uint actionId)
+        public class FishingAction(uint actionId, int minLevel = 1, int gpCost = 0, uint[]? statusProvide = null)
         {
-            public uint ActionId = actionId;
+            public readonly uint    ActionId      = actionId;
+            public readonly int     MinLevel      = minLevel;
+            public readonly int     GpCost        = gpCost;
+            public readonly uint[]? StatusProvide = statusProvide;
+
             public unsafe void UseAction()
             {
                 var amInstance = ActionManager.Instance();
@@ -106,29 +110,53 @@ public partial class AutoGather
             }
         }
 
-        public static readonly BaseAction    Prospect      = new(210, 227, 217, 225);
-        public static readonly BaseAction    Sneak         = new(304, 303, 47);
-        public static readonly BaseAction    TwelvesBounty = new(282, 280, 825, type: EffectType.CrystalsYield);
-        public static readonly BaseAction    Bountiful     = new(4087, 4073, 756, type: EffectType.Yield);
-        public static readonly BaseAction    SolidAge      = new(215, 232, 2765, type: EffectType.Integrity);
-        public static readonly BaseAction    Yield1        = new(222, 239, 219, type: EffectType.Yield);
-        public static readonly BaseAction    Yield2        = new(224, 241, 219, type: EffectType.Yield);
-        public static readonly BaseAction    Truth         = new(221, 238, 221, 222);
-        public static readonly BaseAction    Collect       = new(815, 240);
-        public static readonly BaseAction    Scour         = new(22186, 22182);
-        public static readonly BaseAction    Brazen        = new(22187, 22183);
-        public static readonly BaseAction    Meticulous    = new(22188, 22184);
-        public static readonly BaseAction    Scrutiny      = new(22189, 22185, 757);
-        public static readonly BaseAction    Luck          = new(4095, 4081);
-        public static readonly BaseAction    BountifulII   = new(273, 272, 1286, type: EffectType.Yield);
-        public static readonly BaseAction    GivingLand    = new(4590, 4589, 1802, type: EffectType.CrystalsYield);
-        public static readonly BaseAction    Wise          = new(26522, 26521, type: EffectType.Integrity);
-        public static readonly BaseAction    Gift1         = new(21178, 21177, 2666, type: EffectType.BoonChance);
-        public static readonly BaseAction    Gift2         = new(25590, 25589, 759, type: EffectType.BoonChance);
-        public static readonly BaseAction    Tidings       = new(21204, 21203, 2667, type: EffectType.BoonYield);
+        public static readonly BaseAction Prospect      = new(210, 227, 217, 225);
+        public static readonly BaseAction Sneak         = new(304, 303, 47);
+        public static readonly BaseAction TwelvesBounty = new(282, 280, 825, type: EffectType.CrystalsYield);
+        public static readonly BaseAction Bountiful     = new(4087, 4073, 756, type: EffectType.Yield);
+        public static readonly BaseAction SolidAge      = new(215, 232, 2765, type: EffectType.Integrity);
+        public static readonly BaseAction Yield1        = new(222, 239, 219, type: EffectType.Yield);
+        public static readonly BaseAction Yield2        = new(224, 241, 219, type: EffectType.Yield);
+        public static readonly BaseAction Truth         = new(221, 238, 221, 222);
+        public static readonly BaseAction Collect       = new(815, 240);
+        public static readonly BaseAction Scour         = new(22186, 22182);
+        public static readonly BaseAction Brazen        = new(22187, 22183);
+        public static readonly BaseAction Meticulous    = new(22188, 22184);
+        public static readonly BaseAction Scrutiny      = new(22189, 22185, 757);
+        public static readonly BaseAction Luck          = new(4095, 4081);
+        public static readonly BaseAction BountifulII   = new(273, 272, 1286, type: EffectType.Yield);
+        public static readonly BaseAction GivingLand    = new(4590, 4589, 1802, type: EffectType.CrystalsYield);
+        public static readonly BaseAction Wise          = new(26522, 26521, type: EffectType.Integrity);
+        public static readonly BaseAction Gift1         = new(21178, 21177, 2666, type: EffectType.BoonChance);
+        public static readonly BaseAction Gift2         = new(25590, 25589, 759, type: EffectType.BoonChance);
+        public static readonly BaseAction Tidings       = new(21204, 21203, 2667, type: EffectType.BoonYield);
 
         //Fishing Actions
-        public static readonly FishingAction Cast          = new FishingAction(289);
-        public static readonly FishingAction Quit          = new FishingAction(299);
+        public static readonly FishingAction Cast             = new FishingAction(289);
+        public static readonly FishingAction Quit             = new FishingAction(299);
+        public static readonly FishingAction Hook             = new FishingAction(296);
+        public static readonly FishingAction Chum             = new FishingAction(4104,  5,  100, [763]);
+        public static readonly FishingAction Patience         = new FishingAction(4102,  15, 200, [764, 850]);
+        public static readonly FishingAction PowerfulHookset  = new FishingAction(4103,  15, 50);
+        public static readonly FishingAction PrecisionHookset = new FishingAction(4179,  15, 50);
+        public static readonly FishingAction ThaliaksFavor    = new FishingAction(26804, 15);
+        public static readonly FishingAction Release          = new FishingAction(300,   22);
+        public static readonly FishingAction Mooch            = new FishingAction(297,   25);
+        public static readonly FishingAction Snagging         = new FishingAction(4100,  36,  0,   [761]);
+        public static readonly FishingAction CollectorsGlove  = new FishingAction(4101,  50,  0,   [805]);
+        public static readonly FishingAction FishEyes         = new FishingAction(4105,  57,  550, [762]);
+        public static readonly FishingAction PatienceII       = new FishingAction(4106,  60,  560, [764, 765, 850]);
+        public static readonly FishingAction MoochII          = new FishingAction(268,   63,  100);
+        public static readonly FishingAction VeteranTrade     = new FishingAction(7906,  63,  200);
+        public static readonly FishingAction DoubleHook       = new FishingAction(269,   65,  400);
+        public static readonly FishingAction SurfaceSlap      = new FishingAction(4595,  71,  200, [1803]);
+        public static readonly FishingAction IdenticalCast    = new FishingAction(4596,  79,  350, [1804]);
+        public static readonly FishingAction BaitedBreath     = new FishingAction(26871, 75,  300);
+        public static readonly FishingAction PrizeCatch       = new FishingAction(26806, 81,  200, [2780]);
+        public static readonly FishingAction TripleHook       = new FishingAction(27523, 90,  700);
+        public static readonly FishingAction SparefulHand     = new FishingAction(37045, 91,  100);
+        public static readonly FishingAction BigGameFishing   = new FishingAction(37046, 95,  0,  [3907]);
+        public static readonly FishingAction AmbitiousLure    = new FishingAction(37594, 100, 10, [3972]);
+        public static readonly FishingAction ModestLure       = new FishingAction(37595, 100, 10, [3973]);
     }
 }
