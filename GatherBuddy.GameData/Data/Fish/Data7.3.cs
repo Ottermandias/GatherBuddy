9204b4c499fe094d462dcc088f7c01c9ffb02cbc
caduceator <104291200+caduceator@users.noreply.github.com>
2025-08-07 16:06:27 -0400

    Update Data7.3.cs
    Updating fish data (Lotl hasn't been found yet).

--- GatherBuddy.GameData/Data/Fish/Data7.3.cs
+++ GatherBuddy.GameData/Data/Fish/Data7.3.cs
@@ -8,38 +8,62 @@ public static partial class Fish
     private static void ApplyThePromiseOfTomorrow(this GameData data)
     {
         data.Apply(46188, Patch.ThePromiseOfTomorrow) // Goldentail
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Bite(data, HookSet.Powerful, BiteType.Strong);
         data.Apply(46189, Patch.ThePromiseOfTomorrow) // Prime Adjudicator
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Time(480, 960)
+            .Transition(data, 2)
+            .Weather(data, fog) //placeholder
+            .Bite(data, HookSet.Unknown, BiteType.Legendary);
         data.Apply(46190, Patch.ThePromiseOfTomorrow) // Crenicichla Miyaka
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Time(360, 480)
+            .Transition(data, 1, 2)
+            .Weather(data, rain) //placeholder
+            .Lure(Enums.Lure.Ambitious)
+            .Bite(data, HookSet.Powerful, BiteType.Legendary);
         data.Apply(46191, Patch.ThePromiseOfTomorrow) // Iron Shadowtongue
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Time(960, 1080)
+            .Transition(data, 2, 3, 4)
+            .Weather(data, rain) //placeholder
+            .Bite(data, HookSet.Powerful, BiteType.Legendary);
         data.Apply(46192, Patch.ThePromiseOfTomorrow) // Lotl-in-waiting
             .Bait(data)
             .Bite(data, HookSet.Unknown, BiteType.Unknown);
         data.Apply(46193, Patch.ThePromiseOfTomorrow) // Azure Diver
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43857)
+            .Time(1080, 1440)
+            .Transition(data, 1, 2)
+            .Weather(data, gales) //placeholder
+            .Bite(data, HookSet.Precise, BiteType.Legendary);
         data.Apply(46194, Patch.ThePromiseOfTomorrow) // Sprouting Perch
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Time(1200, 1440)
+            .Transition(data, clouds, umbralstatic) //placeholder
+            .Weather(data, thunderstorms) //placeholder
+            .Bite(data, HookSet.Powerful, BiteType.Legendary);
         data.Apply(46195, Patch.ThePromiseOfTomorrow) // Gigagiant Snakehead
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Time(240, 360)
+            .Predators(data, 350, (43781, 3))
+            .Transition(data, 2, clouds) //placeholder
+            .Weather(data, rain) //placeholder
+            .Bite(data, HookSet.Unknown, BiteType.Legendary);
         data.Apply(46196, Patch.ThePromiseOfTomorrow) // Gondola Louvar
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43859)
+            .Time(480, 720)
+            .Transition(data, rain) //placeholder
+            .Weather(data, 2)
+            .Bite(data, HookSet.Unknown, BiteType.Legendary);
         data.Apply(46249, Patch.ThePromiseOfTomorrow) // Purple Palate
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43858)
+            .Bite(data, HookSet.Powerful, BiteType.Strong);
         data.Apply(46779, Patch.ThePromiseOfTomorrow) // Sunray Ray
-            .Bait(data)
-            .Bite(data, HookSet.Unknown, BiteType.Unknown);
+            .Bait(data, 43857)
+            .Bite(data, HookSet.Powerful, BiteType.Legendary);
     }
     // @formatter:on
 }
+
