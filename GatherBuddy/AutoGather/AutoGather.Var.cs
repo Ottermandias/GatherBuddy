81aaaa861969cea4c0ce7ba0fd296d7daceca2cc
Deathbeyond87 <161655801+Deathbeyond87@users.noreply.github.com>
2025-10-11 01:02:06 -0500

    Hidden/FlyingUnlock Bug Fixes
    Shift gathering addon index and add aethercurrent completion check to ShouldFly

--- GatherBuddy/AutoGather/AutoGather.Var.cs
+++ GatherBuddy/AutoGather/AutoGather.Var.cs
@@ -17,8 +17,11 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Numerics;
 using ECommons;
+using ECommons.DalamudServices;
 using ECommons.MathHelpers;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using GatherBuddy.AutoGather.AtkReaders;
+using LuminaTerritoryType = Lumina.Excel.Sheets.TerritoryType;
 
 namespace GatherBuddy.AutoGather
 {
@@ -88,17 +91,30 @@ namespace GatherBuddy.AutoGather
         public bool ShouldUseFlag
             => !GatherBuddy.Config.AutoGatherConfig.DisableFlagPathing;
 
-        public bool ShouldFly(Vector3 destination)
+        public unsafe bool ShouldFly(Vector3 destination)
         {
             if (Dalamud.Conditions[ConditionFlag.InFlight] || Dalamud.Conditions[ConditionFlag.Diving])
                 return true;
+
+            var territory = Dalamud.ClientState.TerritoryType;
+            var territoryRow = Svc.Data.GameData.GetExcelSheet<LuminaTerritoryType>();
+            if (territoryRow == null)
+                return false;
+
+            var playerState = PlayerState.Instance();
+            if (playerState == null)
+                return false;
+
+            var aetherCurrentComp = territoryRow.GetRow(territory).AetherCurrentCompFlgSet.RowId;
+            if (aetherCurrentComp == 0)
+                return false;
 
             if (GatherBuddy.Config.AutoGatherConfig.ForceWalking || Dalamud.ClientState.LocalPlayer == null)
             {
                 return false;
             }
 
-            return Vector3.Distance(Dalamud.ClientState.LocalPlayer.Position, destination)
+            return playerState->IsAetherCurrentZoneComplete(aetherCurrentComp) && Vector3.Distance(Dalamud.ClientState.LocalPlayer.Position, destination)
              >= GatherBuddy.Config.AutoGatherConfig.MountUpDistance;
         }
 
